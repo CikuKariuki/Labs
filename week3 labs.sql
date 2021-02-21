c0d8124d271d278223faf57e12cedf39cad941ab
CikuKariuki <wanjikukariuki03@gmail.com>
2021-02-21 21:09:39 +0300

    select without from solved

--- week3 labs.sql
+++ week3 labs.sql
@@ -89,3 +89,7 @@ WHERE
     ship_date IS NULL
 ORDER BY order_id DESC;
 
+/* Write a SELECT statement without a FROM clause that creates a row with these columns:
+price 100 (dollars), tax_rate .07 (7 percent), tax_amount(price * tax_rate), total(price + tax_amount). 
+To calculate the fourth column, add the expressions you used for the first and third columns.*/
+select 100 as 'price', 0.07 as 'tax_rate', (100*0.07) as tax_amount, 100+(100*0.07) as total;
